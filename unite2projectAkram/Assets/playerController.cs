b60031368f42c5f3d395724fe45db84b7f0af825
Josecruz777 <756199@student.dusd.net>
2025-03-24 10:23:39 -0700

    jn

--- unite2projectAkram/Assets/playerController.cs
+++ unite2projectAkram/Assets/playerController.cs
@@ -7,7 +7,7 @@ public class playerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-
+    public GameObject projectilPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +17,24 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //LUNCH a projectile from the player
+            Instantiate(projectilPrefab, transform.position, projectilPrefab.transform.rotation);
+        }
+
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-            //keep the player in bounds
-            horizontalInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-            if (transform.position.x < -xRange)
-            {
-                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-            }
+        }
+        //keep the player in bounds
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
     }
 }
+
+
