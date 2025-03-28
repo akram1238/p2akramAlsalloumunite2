909f0aa4acb933c4a0e216cf4499cb3a2b9534cb
Josecruz777 <756199@student.dusd.net>
2025-03-28 09:57:57 -0700

    making the animals
    repeat

--- unite2projectAkram/Assets/DestroyOutOfBounds.cs
+++ unite2projectAkram/Assets/DestroyOutOfBounds.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
-    private
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (transform.position.z > topBound)
-        { Destroy(gameObject); }
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        { 
+            Destroy(gameObject); 
+        }
     }
 }
